66c67e93e3b15e45fa6f18f87848e26f3f1e39da
1103568970 <1103568970@qq.com>
2017-10-19 21:25:59 +0800

    first commit

---
+++ 网页/js/heads.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2017/9/27.
+ */
+
+
+window.onload=function(){
+    var see = document.querySelector("#see-case");
+    var seec = document.querySelector("#see-cast-study");
+    var seet = document.querySelector("#see-study");
+
+    console.log(see);
+    seec.onclick=()=>{
+        alert("网站正在努力建设中，请五年后再试")
+    }
+    seet.onclick=()=>{
+        alert("网站正在努力建设中，请五年后再试")
+    }
+    see.onclick=()=>{
+        alert("网站正在努力建设中，请五年后再试")
+    }
+}
+
+
+
